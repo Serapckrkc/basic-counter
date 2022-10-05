079a2df4883d2efa31662dd1d51d9337883b6a56
Serap Acıpayam <Serapckrkc@gmail.com>
2022-10-05 14:34:52 +0300

    Buttons and setState added

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+ class Counter extends Component {
+    state = {
+        number:0
+    }
+    decrement = () =>{
+        this.setState({
+            number: -- this.state.number
+        })
+    }
+    increment = () =>{
+        this.setState({
+            number: ++ this.state.number
+        })
+    }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.number}</h1>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}>+</button>
+      </div>
+    )
+  }
+}
+export default Counter;
